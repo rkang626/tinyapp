f6a1367957f0d00e988352c24f67b59472ef4b52
Ryan Kang <rkang626@gmail.com>
2021-01-19 21:47:38 +0000

    allow users to edit and delete URLs

--- express_server.js
+++ express_server.js
@@ -67,6 +67,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:shortURL/update", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
